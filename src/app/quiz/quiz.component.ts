fe8c2c3a7b74896e22eadaae0d124c5d45e9c548
Sofien <sofien.abdeddaim@gmail.com>
2021-01-01 20:47:51 +0100

    store best score in session

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-quiz',
@@ -9,7 +9,33 @@ export class QuizComponent implements OnInit{
 
   @Input() quizQuestions;
 
+  bestScore;
+  question;
+  timeOut = false;
+  currentScore = 0;
+  questionIndex = 0;
+
   ngOnInit(): void {
     console.log(this.quizQuestions);
+    if (sessionStorage.getItem('CK:best-score')) {
+      this.bestScore = +sessionStorage.getItem('CK:best-score');
+    } else {
+      this.bestScore = 0;
+    }
+    this.question = this.quizQuestions[this.questionIndex];
+  }
+
+  trueAnswer(val): void {
+    this.currentScore = val ? ++this.currentScore : this.currentScore;
+    this.bestScore = this.currentScore > this.bestScore ? this.currentScore : this.bestScore;
+    this.question = this.quizQuestions[++this.questionIndex];
+  }
+
+  gameOver(): void {
+    this.timeOut = true;
+    if (this.currentScore >= this.bestScore) {
+      this.bestScore = this.currentScore;
+      sessionStorage.setItem('CK:best-score', this.bestScore.toString());
+    }
   }
 }
