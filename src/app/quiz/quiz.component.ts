2c8a32996b75e47b20f19befbe6e77e07e7ae70c
Sofien <sofien.abdeddaim@gmail.com>
2021-01-02 00:44:52 +0100

    add retry functionality

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { CountdownComponent } from './countdown/countdown.component';
 
 @Component({
   selector: 'app-quiz',
@@ -7,16 +8,17 @@ import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit{
 
+  @ViewChild(CountdownComponent, { static: false }) countdownComponentRef: CountdownComponent;
   @Input() quizQuestions;
 
   bestScore;
   question;
+  resetTime;
   timeOut = false;
   currentScore = 0;
   questionIndex = 0;
 
   ngOnInit(): void {
-    console.log(this.quizQuestions);
     if (sessionStorage.getItem('CK:best-score')) {
       this.bestScore = +sessionStorage.getItem('CK:best-score');
     } else {
@@ -28,6 +30,9 @@ export class QuizComponent implements OnInit{
   trueAnswer(val): void {
     this.currentScore = val ? ++this.currentScore : this.currentScore;
     this.bestScore = this.currentScore > this.bestScore ? this.currentScore : this.bestScore;
+    if (this.questionIndex >= this.quizQuestions.length - 1) {
+      this.questionIndex = 0;
+    }
     this.question = this.quizQuestions[++this.questionIndex];
   }
 
@@ -37,5 +42,11 @@ export class QuizComponent implements OnInit{
       this.bestScore = this.currentScore;
       sessionStorage.setItem('CK:best-score', this.bestScore.toString());
     }
+  }
+
+  retry(): void {
+    this.timeOut = false;
+    this.currentScore = 0;
+    this.countdownComponentRef.countdown = 60;
   }
 }
